a5c4f033f2f45b4cdc9554fc7d42f82e6a7ec25c
rdaforno <reto.daforno@tik.ee.ethz.ch>
2019-05-08 11:06:11 +0200

    minor adjustments

--- apps/glossy/glossy-test.c
+++ apps/glossy/glossy-test.c
@@ -10,7 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- *
  * 3. Neither the name of the copyright holder nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
@@ -149,8 +148,8 @@ PT_THREAD(glossy_thread(rtimer_t *rt))
           sync_state = 0;
           continue;
         }
-        if(glossy_get_relay_cnt_first_rx() > max_hop) {
-          max_hop = glossy_get_relay_cnt_first_rx();
+        if(glossy_get_relay_cnt() > max_hop) {
+          max_hop = glossy_get_relay_cnt();
         }
         /* print out some stats */
         DEBUG_PRINT_INFO("rcv=%u miss=%u boot=%u per=%u "
